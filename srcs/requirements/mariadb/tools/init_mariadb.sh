3d76a3a0247e8894da9a8d6a3ad52422fce2bbdc
Remi-cpn <cpnpro@outlook.fr>
2026-08-12 11:30:04 -0400

    Correction sur mariadb

--- srcs/requirements/mariadb/tools/init_mariadb.sh
+++ srcs/requirements/mariadb/tools/init_mariadb.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
-
+set -e
 # Cree un dossier necessaire a mairadb
 mkdir -p /run/mysqld
 # Donne les droits a ce dossier
 chown -R mysql:mysql /run/mysqld
 
-# Regarder si la base existe
-if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
-	mysqld &
-	# attente lancement de mysqld
-	until mysqladmin ping --silent; do
-    		sleep 1
-	done
-	# <<- Le - permet d'ignorer les tabulations
-	mysql <<- EOF
-	CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-	CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-	ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-	GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-	FLUSH PRIVILEGES;
-	EOF
-	# Arret du server tmp avec les droits
-	mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
+if [ ! -d /var/lib/mysql/wordpress ]; then
+	envsubst < /usr/local/bin/init.sql.template > /tmp/init.sql
 fi
 
-
-exec mysqld
+exec mariadbd --init-file=/tmp/init.sql
